6cb60408aa0e927428b2262c65dd971c93ece2da
gerryS2xda <Qwerty96xda>
2020-05-18 10:21:41 +0200

    Add code for read csv (with load_fraud_data() function) and test some DataFrame function like info(), head()

--- getdata/fetchdata.py
+++ getdata/fetchdata.py
@@ -1,6 +1,15 @@
 import pandas as pd
 
 PATH1='C:\dataset'
-ds=pd.read_csv(PATH1+'\datasetml.csv')
-ds.head();
+ABSOLUTE_PATH1 = 'C:/Users/gerar/PycharmProjects/FraudDetection_FDSML/'
+FRAUD_DSPATH = ABSOLUTE_PATH1 + "dataset/FinancialDatasetForFraudDetection.csv"
+
+def load_fraud_data(fraud_path = FRAUD_DSPATH):
+    return pd.read_csv(fraud_path)
+
+#Call function
+fraudata = load_fraud_data()
+fraudata.info()
+print(fraudata.head(10)) #necessario perche' PyCharm non e' Python Shell
+
 
