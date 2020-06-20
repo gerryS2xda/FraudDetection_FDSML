b7a5142814ed0da56cbea30a4e0c5c11e1740c50
bernardino716 <45485761+bernardino716@users.noreply.github.com>
2020-06-20 21:20:57 +0200

    aggiuntadescribe

--- getdata/getdatainsights.py
+++ getdata/getdatainsights.py
@@ -5,12 +5,15 @@ from matplotlib import pyplot as plt
 #Call function
 print('Loading data...')
 fraudata = fetchdata.load_fraud_data()
+
 print('Head:')
 print(fraudata.head(10)) #necessario perche' PyCharm non e' Python Shell
 print('Info:')
-fraudata.info()
+fraudata.info(null_counts=True) #
 print('Categorical:')
 print(fraudata["type"].value_counts())
+print('Describe:')
+print(fraudata.describe())
 print('Istogrammi:')
 
 fraudata.plot(kind='bar')
