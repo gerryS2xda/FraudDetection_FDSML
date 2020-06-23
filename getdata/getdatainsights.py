ea5daea39964d75409f8e8bdfd8f372e7ecbc43b
bernardino716 <45485761+bernardino716@users.noreply.github.com>
2020-06-23 20:33:56 +0200

    modifiche istogrammi a getdatainsights

--- getdata/getdatainsights.py
+++ getdata/getdatainsights.py
@@ -22,19 +22,23 @@ print('Istogramma amount:')
 
 fraudata["amount_cat"]=pd.cut(fraudata["amount"],bins=[0,1000,10000,50000,100000,200000,500000,1000000,5000000,np.inf],labels=[0,1,2,3,4,5,6,7,8])
 fraudata["amount_cat"].hist(bins=50,figsize=(10,5))#bins e' il numero di valori differenti
+plt.title("amount_cat")
 print("Statistiche delle categorie")
 print(fraudata["amount_cat"].value_counts())
 plt.figure(2)
 print('Istogramma type:')
 fraudata["type"].hist(bins=50,figsize=(10,5))#bins e' il numero di valori differenti
+plt.title("type")
 print("Statistiche delle categorie")
 print(fraudata["type"].value_counts())
 plt.figure(3)
 print('Istogramma isFraud:')
 fraudata["isFraud"].hist(bins=50,figsize=(10,5))#bins e' il numero di valori differenti
+plt.title("isFraud")
 plt.figure(4)
 print('Istogramma isFlaggedFraud')
-fraudata["isFraud"].hist(bins=50,figsize=(10,5))#bins e' il numero di valori differenti
+fraudata["isFlaggedFraud"].hist(bins=50,figsize=(10,5))#bins e' il numero di valori differenti
+plt.title("isFlaggedFraud")
 plt.show()
 print("Creo test set stratificato")
 train_set,test_set=createstratifiedtestset.stratified_test_fraud_data()
