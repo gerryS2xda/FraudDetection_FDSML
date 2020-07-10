4562c09e95cb1810ea4df797596015f1cc452815
bernardino716 <45485761+bernardino716@users.noreply.github.com>
2020-07-10 20:30:43 +0200

    baseline Decision Tree

---
+++ baselines/baselineDecisionTree.py
@@ -0,0 +1,63 @@
+from getdata import fetchdata
+from datapreparation import createstratifiedtestset
+from visualizationresult import visualizeROC,visualizeconfusionmatrix
+
+from sklearn.tree import DecisionTreeClassifier
+import numpy as np
+
+import matplotlib.pyplot as plt
+from sklearn.tree import export_graphviz
+
+
+from sklearn.metrics import classification_report
+from sklearn.metrics import accuracy_score
+
+from sklearn.metrics import mean_squared_error
+
+fraudata = fetchdata.load_fraud_data()
+train_set,test_set = createstratifiedtestset.stratified_test_fraud_data()
+
+train_set_labels1 = train_set["isFraud"].copy()
+train_set_predictive=train_set.drop(['type',"nameOrig","nameDest",'isFraud','isFlaggedFraud'],axis=1)
+
+test_set_labels1 = test_set["isFraud"].copy()
+test_set_predictive=test_set.drop(['type',"nameOrig","nameDest",'isFraud','isFlaggedFraud'],axis=1)
+tree_clf = DecisionTreeClassifier(max_depth=3,random_state=42)
+decision_tree_trained=tree_clf.fit(train_set_predictive, train_set_labels1)
+
+export_graphviz(tree_clf, out_file="decisiontree1.dot",
+    feature_names=['step','amount','oldbalanceOrg','newbalanceOrg','oldbalanceDest','newbalanceDest'],
+    class_names=['0','1'],
+    rounded=True, filled=True
+)
+
+
+
+visualizeROC.create_roc_curve1(decision_tree_trained,test_set_predictive,test_set_labels1)
+
+
+y_pred = tree_clf.predict(test_set_predictive)
+print(y_pred)
+
+print("accuracy:")
+print(accuracy_score(test_set_labels1,y_pred))
+report=classification_report(y_pred, test_set_labels1)
+print("Report:")
+print(report)
+
+visualizeconfusionmatrix.confusion_matrix1(tree_clf,test_set_predictive,test_set_labels1,"Decision Tree")
+
+X_train=train_set_predictive
+X_val=test_set_predictive
+y_train=train_set_labels1
+y_val = test_set_labels1
+model=tree_clf
+train_errors, val_errors = [], []
+for m in range(1, len(X_train)):
+        model.fit(X_train[:m], y_train[:m])
+        y_train_predict = model.predict(X_train[:m])
+        y_val_predict = model.predict(X_val)
+        train_errors.append(mean_squared_error(y_train_predict, y_train[:m]))
+        val_errors.append(mean_squared_error(y_val_predict, y_val))
+        plt.plot(np.sqrt(train_errors), "r-+", linewidth=2, label="train")
+        plt.plot(np.sqrt(val_errors), "b-", linewidth=3, label="val")
