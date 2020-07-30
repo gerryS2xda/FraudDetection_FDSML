7c6567b2b9f347603cccec987a8b5828b63eb027
bernardino716 <45485761+bernardino716@users.noreply.github.com>
2020-07-30 13:29:30 +0200

    Resampling

--- getdata/resampling.py
+++ getdata/resampling.py
@@ -4,8 +4,8 @@ from getdata import fetchdata
 
 def resamplingDataset():
     fraudata = fetchdata.load_fraud_data()
-    df_class_1_over = fraudata[fraudata["isFraud"]==1].sample(3000000, random_state=42,replace=True)
-    df_0=resample(fraudata[fraudata["isFraud"]==0],n_samples=3000000,replace=True,random_state=42)
-    df_resampled = pd.concat([df_0, df_class_1_over], axis=0)
+    df_class_0_under = fraudata[fraudata["isFraud"] == 0].sample(3000000, random_state=42)
+    df_1_over = resample(fraudata[fraudata["isFraud"] == 1], n_samples=3000000, random_state=42)
+    df_resampled = pd.concat([df_class_0_under, df_1_over], axis=0)
     return df_resampled
     
