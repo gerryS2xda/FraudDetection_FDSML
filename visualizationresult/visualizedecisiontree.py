0819dadcf414071b81af7e5ce89d1d738b538be2
bernardino716 <45485761+bernardino716@users.noreply.github.com>
2020-08-03 20:47:55 +0200

    Delete visualizedecisiontree.py

--- visualizationresult/visualizedecisiontree.py
+++
@@ -1,8 +0,0 @@
-"""Visualizza l'albero di decisione"""
-from sklearn import tree
-from matplotlib import pyplot as plt
-
-# Funzione che visualizza l'albero di decisione
-def decision_tree(clf):
-    plt.figure(3)
-    tree.plot_tree(clf)
