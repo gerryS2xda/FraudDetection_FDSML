a6fda73dcedf7b4cd624592fc31f8cca6aafdfe4
bernardino716 <45485761+bernardino716@users.noreply.github.com>
2020-08-03 20:02:37 +0200

    Add files via upload

--- model/DatasetResampled/KNNTunedResampled.py
+++ model/DatasetResampled/KNNTunedResampled.py
@@ -1,16 +1,16 @@
 """K-NN fine-tuned sul dataset resampled"""
 from sklearn.metrics import accuracy_score, classification_report
 from sklearn.neighbors import KNeighborsClassifier
-from datapreparation import preparetraintestresampled
-from visualizationresult import visualizeROC, visualizeconfusionmatrix
+from Progetto.datapreparation.preparedata import preparetraintestresampled
+from Progetto.visualizationresult import visualizeROC, visualizeconfusionmatrix
 
-train_set,test_set,train_labels,test_labels=preparetraintestresampled.prepared_fraud_data()
+train_set,test_set,train_labels,test_labels= preparetraintestresampled.prepared_fraud_data()
 
 knn_clf=KNeighborsClassifier(n_neighbors=3,metric="euclidean",n_jobs=-1)
 knn_trained=knn_clf.fit(train_set, train_labels)
 
 # Creazione della ROC Curve
-visualizeROC.create_roc_curve1(knn_trained,test_set,test_labels)
+visualizeROC.create_roc_curve1(knn_trained, test_set, test_labels)
 
 
 y_pred = knn_clf.predict(test_set)
@@ -23,5 +23,5 @@ print("Report:")
 print(report)
 
 # Creazione della ROC Curve
-visualizeconfusionmatrix.confusion_matrix1(knn_clf,test_set,test_labels,"KNN")
+visualizeconfusionmatrix.confusion_matrix1(knn_clf, test_set, test_labels, "KNN")
 
